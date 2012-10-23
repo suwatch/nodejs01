b82728e3190c5148fbc82587758c1652927ea5b3
Suwat Ch <suwatch@microsoft.com>
2012-10-22 19:36:41 -0700

    fix trace format

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-var http = require('http'),
-    connect = require('connect');
+var http = require('http');
 
-var app = connect()
-  .use(function(req, res){
-      console.log('Received request at ' + new Date().toString() + '\r\n');
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Hello from nodejs01 site at ' + new Date().toString() + '!\n');
-  });
-
-//setInterval(function writeToLog(){
-//      console.log(new Date().toString() + '\r\n');
-//}, 5000);
+var app = function(req, res) {
+    console.log(new Date().toString() + ': request for ' + req.url);
+    
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    res.end('Hello from nodejs01 site at ' + new Date().toString() + '!\n');
+};
 
 http.createServer(app).listen(process.env.port || 8000, 'localhost');
